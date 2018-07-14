aea3d03a887dc56db882913cd476b28c1feb5623
ismboedev <ismboedev@gmail.com>
2018-07-14 15:18:06 +0200

    Remote for Samsung TVs

--- tencereRemote.py
+++ tencereRemote.py
@@ -3,7 +3,7 @@
 from subprocess import Popen, PIPE
 from xdo import Xdo
 
-# define function to send keys to a window w
+# define function to send keys to a window in list
 def sendkey( list, str ):
     for i in range( 0, len(list) ):
         xdo.send_keysequence_window( list[i], str.encode(), delay=10 )
@@ -29,34 +29,42 @@ with Popen('cec-client', stdout=PIPE, universal_newlines=True) as cec:
 
         # channel up 
         if 'key pressed: channel up' in line and 'duration' in line:
+            sendkey( urxvt, "w" )
             continue
 
         # channel down
         if 'key pressed: channel down' in line and 'duration' in line:
+            sendkey( urxvt, "s" )
             continue
 
         # channels selection
         if 'key pressed: channels list' in line and 'duration' in line:
+            sendkey( urxvt, "l" )
             continue
 
         # previous channel
         if 'key pressed: pre' in line and 'duration' in line:
+            sendkey( urxvt, "p" )
             continue
 
         # guide key to regenerate streamlink urls
         if 'key pressed: electronic' in line and 'duration' in line:
+            sendkey( urxvt, "g" )
             continue
 
         # press exit to stop the player
         if 'key pressed: exit' in line and 'duration' in line:
+            sendkey( urxvt, "e" )
             continue
 
         # stop does nothing
         if 'key pressed: stop' in line and 'duration' in line:
+            sendkey( urxvt, "t" )
             continue
 
         # green key cycles through the sources for that channel
         if 'key pressed: F3' in line and 'duration' in line:
+            sendkey( urxvt, "c" )
             continue
 
         # when TV is powered off
