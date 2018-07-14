533b51d8cc671c45ac9eeff16b0e8d6014bd995f
ismboedev <ismboedev@gmail.com>
2018-07-14 23:43:50 +0200

    added urwid menu, limited functionality

--- tencereTV.py
+++ tencereTV.py
@@ -1,8 +1,9 @@
 #!/bin/python
 
-import yaml
-from streamlink import Streamlink
 import time
+from streamlink import Streamlink
+import yaml
+import urwid
 
 
 # print into a logfile for debugging, or to have a live overvier whats
@@ -47,18 +48,51 @@ class Channel:
     def url(self):
         return self.URLs[self.iterator]
 
-    
+
+# def urwid menu and functions.
+def menu( title, ch_list ):
+    body = [urwid.Text( title, 'center'), urwid.Divider() ]
+    for i in range( 0, len(ch_list) ):
+        if ( i < 10 ):
+            fil1 = "0"
+            fil2 = "   "
+        else:
+            fil1 = ""
+            fil2 = "  "
+        button = urwid.Button( fil1 + str(i) + fil2 + ch_list[i].name )
+        urwid.connect_signal( button, 'click', ch_selected, i )
+        body.append( urwid.AttrMap( button, None, focus_map='reversed' ) )
+    return urwid.ListBox( urwid.SimpleFocusListWalker( body ) )
+
+
+# this starts the selected channel
+def ch_selected( button, sel ):
+    if sel == 0:
+        printlog( "selected first one" )
+
+
+# this function is controlling everything
+def inp( key ):
+    if key in ('q', 'Q'):
+        printlog( "exiting tencere" )
+        raise urwid.ExitMainLoop()
 
 
 # read channels from tvsources.yaml
 ch_list = []
-
 with open("tvsources.yaml", 'r') as ts:
     for data in yaml.load_all( ts ):
         ch_list.append( Channel(data) )
 
-print(ch_list)
 
-for i in range(0, len(ch_list) ):
-    #ch_list[i].getURLs
-    print( ch_list[i].URLs )
+
+printlog("starting menu ...")
+main = urwid.Padding( menu( u'channels', ch_list ), left=4, right=4 )
+
+top = urwid.Overlay( main, urwid.SolidFill( u'\N{MEDIUM SHADE}' ),
+    align='center', width=( 'relative', 70 ),
+    valign='middle', height=( 'relative', 70 ),
+    min_width=20, min_height=9 )
+
+men = urwid.MainLoop( top, unhandled_input=inp, palette=[('reversed', 'standout, dark green, italics', '')])
+men.run()
