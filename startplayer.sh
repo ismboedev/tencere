a13f831195405bdc30112cefdcd3edcdf66086d6
ismboedev <ismboedev@gmail.com>
2018-07-22 14:57:38 +0200

    added mpv class and input funtionality

--- startplayer.sh
+++ startplayer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-	mpv --idle=yes --input-ipc-server=/tmp/mpvsocket --volume=100 --osd-duration 10000 --osd-font-size=150 --osd-color='#00FF00' --osd-align-x=right --osd-align-y=center
+	mpv --idle=yes --input-ipc-server=/tmp/mpvsocket --volume=100 --osd-duration 10000 --osd-font-size=150 --osd-color='#00FF00' --osd-align-x=right --osd-align-y=center --loop-file=inf --no-input-default-bindings
 	sleep 1s
 done
--- tencereTV.py
+++ tencereTV.py
@@ -3,6 +3,7 @@
 import time
 from streamlink import Streamlink
 import yaml
+from subprocess import Popen, PIPE
 import urwid
 
 
@@ -12,6 +13,9 @@ def printlog( str ):
     with open( 'tenc.log', mode='a' ) as log:
         now = time.strftime("%Y-%m-%d %H:%M", time.localtime())
         log.write("[" + now + "]" + "\t " + str + "\n")
+    return 0
+
+
 
 
 # define channel class, that gets streamlink urls at init
@@ -33,6 +37,7 @@ class Channel:
     def getURLs(self):
         streamlink = Streamlink()
         printlog("------ \t " + self.name + "\t ------" )
+        self.iterator = len( self.sources) - 1
         for i in range( 0, len(self.sources) ):
             try:
                 self.URLs[i] = streamlink.streams( self.sources[i] )['best'].url
@@ -44,38 +49,163 @@ class Channel:
                 #self.URLs.append( 'nosource.mp4' )
         return 0
 
-
     def url(self):
         return self.URLs[self.iterator]
+
+
+
+
+
+# class mpv
+class Mpv:
+
+    load1 = str( "echo \'{ \"command\": [\"loadfile\", \"" )
+    show1 = str( "echo \'{ \"command\": [\"show-text\", \"" )
+    stop1 = str( "echo \'{ \"command\": [\"stop" )
+    com2 = str( "\"] }\' | socat - /tmp/mpvsocket" )
+
+    def __init__( self ):
+        # start mpv player in background and give it some time to start
+        # listening to /tmp/mpvsocket
+        #try:
+        #    call( 'pkill startplayer.sh && pkill mpv', shell=True, universal_newlines=True )
+        #except:
+        #    printlog( "no previous MPV to kill" )
+
+        #Popen( './startplayer.sh', stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True )
+        printlog( "init MPV player" )
+        #time.sleep( 2 )
+
+    def load( self, str ):
+        x = "%s%s%s" % ( self.load1, str, self.com2 )
+        Popen( x, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True )
+        time.sleep( 1 )
+        return 0
+
+    def show( self, str ):
+        x = "%s%s%s" % ( self.show1, str, self.com2 )
+        Popen( x, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True )
+        return 0
+
+    def stop( self ):
+        x = "%s%s" % ( self.stop1, self.com2 )
+        Popen( x, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True )
+        return 0
+
+
+
+
+
 
 
 # def urwid menu and functions.
 def menu( title, ch_list ):
     body = [urwid.Text( title, 'center'), urwid.Divider() ]
     for i in range( 0, len(ch_list) ):
-        if ( i < 10 ):
+        if ( i < 9 ):
             fil1 = "0"
             fil2 = "   "
         else:
             fil1 = ""
             fil2 = "  "
-        button = urwid.Button( fil1 + str(i) + fil2 + ch_list[i].name )
+        button = urwid.Button( fil1 + str( i + 1 ) + fil2 + ch_list[i].name )
         urwid.connect_signal( button, 'click', ch_selected, i )
         body.append( urwid.AttrMap( button, None, focus_map='reversed' ) )
     return urwid.ListBox( urwid.SimpleFocusListWalker( body ) )
 
 
+
+
+# introduce variables
+curr_ch = 0
+temp_ch = 0
+pre_ch = 0
+
+
+
+
+
+
 # this starts the selected channel
 def ch_selected( button, sel ):
-    if sel == 0:
-        printlog( "selected first one" )
+    global curr_ch
+    global temp_ch
+    global pre_ch
+    pre_ch = curr_ch
+    curr_ch = sel
+    printlog( "Channel " + str(curr_ch) + " - " + ch_list[curr_ch].name + "  -  source: " + str( ch_list[curr_ch].iterator ) )
+    mpv.load( ch_list[curr_ch].url() )
+    mpv.show( str( curr_ch + 1 ) )
 
 
 # this function is controlling everything
 def inp( key ):
-    if key in ('q', 'Q'):
-        printlog( "exiting tencere" )
-        raise urwid.ExitMainLoop()
+    global curr_ch
+    global temp_ch
+    global pre_ch
+    # quit
+    #if key in ( 'q', 'Q' ):
+    #    printlog( "exiting tencere" )
+    #    raise urwid.ExitMainLoop()
+
+    if key in ( 'q', 'Q' ):
+        mpv.stop()
+
+
+    # ch up
+    if key in ( 'w', 'W' ):
+        pre_ch = curr_ch
+        curr_ch = ( curr_ch + 1 + len( ch_list ) ) % len( ch_list )
+        printlog( "Channel " + str(curr_ch) + " - " + ch_list[curr_ch].name + "  -  source: " + str( ch_list[curr_ch].iterator ) )
+        mpv.load( ch_list[curr_ch].url() )
+        mpv.show( str( curr_ch + 1 ) )
+
+    # ch down
+    if key in ( 's', 'S' ):
+        pre_ch = curr_ch
+        curr_ch = ( curr_ch - 1 + len( ch_list ) ) % len( ch_list )
+        printlog( "Channel " + str(curr_ch) + " - " + ch_list[curr_ch].name + "  -  source: " + str( ch_list[curr_ch].iterator ) )
+        mpv.load( ch_list[curr_ch].url() )
+        mpv.show( str( curr_ch + 1 ) )
+
+    # channels list
+    if key in ( 'l', 'L' ):
+        mpv.stop()
+
+    # previous channel
+    if key in ( 'p', 'P' ):
+        temp_ch = curr_ch
+        curr_ch = pre_ch
+        pre_ch = temp_ch
+        printlog( "Channel " + str(curr_ch) + " - " + ch_list[curr_ch].name + "  -  source: " + str( ch_list[curr_ch].iterator ) )
+        mpv.load( ch_list[curr_ch].url() )
+        mpv.show( str( curr_ch + 1 ) )
+
+    # refresh URLs
+    if key in ( 'g', 'G' ):
+        mpv.stop()
+        printlog( "Refreshing sources" )
+        for i in range( 0, len(ch_list) ):
+            ch_list[i].getURLs()
+        printlog( "Channel " + str(curr_ch) + " - " + ch_list[curr_ch].name + "  -  source: " + str( ch_list[curr_ch].iterator ) )
+        mpv.load( ch_list[curr_ch].url() )
+        mpv.show( str( curr_ch + 1 ) )
+
+    # exit mpv, show list
+    if key in ( 'e', 'E' ):
+        mpv.stop()
+
+    # change source
+    if key in ( 'c', 'C' ):
+        ch_list[curr_ch].iterator = ( ch_list[curr_ch].iterator + 1 + \
+                len( ch_list[curr_ch].sources ) ) % len( ch_list[curr_ch].sources )
+        printlog( "Channel " + str(curr_ch) + " - " + ch_list[curr_ch].name + "  -  source: " + str( ch_list[curr_ch].iterator ) )
+        mpv.load( ch_list[curr_ch].url() )
+        mpv.show( str( curr_ch + 1 ) )
+
+    time.sleep( 1 )
+
+    
 
 
 # read channels from tvsources.yaml
@@ -86,6 +216,14 @@ with open("tvsources.yaml", 'r') as ts:
 
 
 
+# start MPV
+mpv = Mpv()
+
+
+
+
+
+# start mainloop with urwid
 printlog("starting menu ...")
 main = urwid.Padding( menu( u'channels', ch_list ), left=4, right=4 )
 
